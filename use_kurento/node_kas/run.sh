461f180e638d6eda7b6422ea128c97c55f349898
lrobot <lrobot.qq@gmail.com>
2024-10-09 18:19:53 +0800

    info

--- use_kurento/node_kas/run.sh
+++ use_kurento/node_kas/run.sh
@@ -1,6 +1,16 @@
 
 docker build -f Dockerfile .
+
+while true; do
 docker run --name nodekas --network host -it --rm `docker build -q -f Dockerfile .`
+read -t 3 -p "Press enter to exit:(timeout in 3s will autorestart)" RESP
+if [[ $? -gt 128 ]] ; then
+    echo -e "\nTimeout, restarting..."
+else
+    echo "Response = \"$RESP\""  # adding quotes so empty strings are obvious
+    break
+fi
+done
 
 
 
